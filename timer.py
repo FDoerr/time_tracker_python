2a1cec3f2b030a10baa3f1135361f9b4b29431cb
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-10-04 21:39:37 +0200

    removed debug statements

--- timer.py
+++ timer.py
@@ -1,6 +1,5 @@
 import time
 
-
 class Timer:   
         
     def __init__(self):
@@ -8,34 +7,24 @@ class Timer:
         self.start_time: float   = 0.0
         self.current_time: float = 0.0
         self.elapsed_time: float = 0.0
-        print('initialized')
 
     def start(self):
         self.start_time = time.time() - self.elapsed_time
-        self.running = True  
-        print('start')      
+        self.running = True      
 
     def stop(self):
         self.running= False
-        print('stop')
 
     def reset(self):
         self.start_time = 0.0
         self.current_time = 0.0
         self.elapsed_time = 0.0
-        print('reset')
 
     def get_elapsed_time(self):
         self.update_timer()
         self.elapsed_time = self.current_time - self.start_time
-        print('get time')
         return round(self.elapsed_time)
 
     def update_timer(self):
         if self.running == True:            
-            self.current_time = time.time()
-        print('update timer')
-            
- #use tkinter after() method to delay the update in gui this keeps ui responsive
-
- 
+            self.current_time = time.time()   
