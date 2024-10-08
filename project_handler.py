40544fa0ebc64bdf9b62e12542a7d1d2b14ca4f3
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-10-08 22:30:30 +0200

    added notes

--- project_handler.py
+++ project_handler.py
@@ -42,14 +42,80 @@ Project Name
         time in session
         topic
     ...
-    
-   
+[name, Date, time, topic]
+
+{'projects': 
+        [{ 'project_name': 'Projekt 1',
+            'session_data': [
+                            {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
+                            {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
+                            ]
+        },
+        { 'project_name': 'Projekt 1',
+            'session_data': [
+                            {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
+                            {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
+                            ]
+        }
+        }]
+}
+
+{'projects': 
+        {
+        'project_id': 'Projekt 1',
+        'session_data':
+                    {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
+        }
+}
+
 
 '''
-session_data: dict  = {'Session Date' : 'Datum und Uhrzeit',
+session_data1: dict  = {'Session Date' : '1 Datum und Uhrzeit',
                         'Session time': 10000,
-                        'topic' : 'Todo-List point'}
-project_dict: dict  = {'project_name' : 'Projekt 1', 'session_data' : session_data}
-projects_dict: dict = {'projects' : project_dict}
+                        'topic' : '1 Todo-List point'}
+session_data2: dict  = {'Session Date' : '2 Datum und Uhrzeit',
+                        'Session time': 20000,
+                        'topic' : '2 Todo-List point'}
+project_dict1: dict  = {'project_name' : 'Projekt 1', 'session_data' : [session_data1, session_data2]}
+project_dict2: dict  = {'project_name' : 'Projekt 1', 'session_data' : [session_data1, session_data2]}
+projects_dict: dict  = {'projects' : [project_dict1, project_dict2]}
 
 print(projects_dict)
+
+dictionary:dict  = {
+    "projects": [
+      {
+        "project_name": "Projekt 1",
+        "session_data": [
+          {
+            "Session Date": "1 Datum und Uhrzeit",
+            "Session time": 10000,
+            "topic": "1 Todo-List point"
+          },
+          {
+            "Session Date": "2 Datum und Uhrzeit",
+            "Session time": 20000,
+            "topic": "2 Todo-List point"
+          }
+        ]
+      },
+      {
+        "project_name": "Projekt 1",
+        "session_data": [
+          {
+            "Session Date": "1 Datum und Uhrzeit",
+            "Session time": 10000,
+            "topic": "1 Todo-List point"
+          },
+          {
+            "Session Date": "2 Datum und Uhrzeit",
+            "Session time": 20000,
+            "topic": "2 Todo-List point"
+          }
+        ]
+      }
+    ]
+  }
+
+if dictionary == projects_dict:
+    print(True)
