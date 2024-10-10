3fe760f0aa60aa312022b33d2edfa65257e5a150
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-10-10 14:20:39 +0200

    revamped notes for use of database

--- project_handler.py
+++ project_handler.py
@@ -1,121 +1,83 @@
 '''
-project_handler:
 
+project_handler:
 list Projects in combobox
 create new Projects via combobox
-store those projects in a file
+store those projects in a file/database
 retrieve those projects on startup
+build database if nonexistent
 
-
+Combobox:
+focusin -> load list from databse
+return  -> save new item to database
+Button:
 save time to currently active Project via Save button
 
 
-focusin -> load list from file
-return  -> save new item to file
-
-
-data i need:
+data i need: [name, Date, time, task]
 Project Name
     session date + current time
         time in session
-        topic
+        task
 
     session date + current time
         time in session
-        topic
+        task
     
     session date + current time
         time in session
-        topic
+        task
     ...
 
 Project Name
     session date + current time
         time in session
-        topic
-
-    session date + current time
-        time in session
-        topic
-    
-    session date + current time
-        time in session
-        topic
+        task
     ...
-[name, Date, time, topic]
 
-{'projects': 
-        [{ 'project_name': 'Projekt 1',
-            'session_data': [
-                            {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
-                            {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
-                            ]
-        },
-        { 'project_name': 'Projekt 1',
-            'session_data': [
-                            {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
-                            {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
-                            ]
-        }
-        }]
-}
+        
 
-{'projects': 
-        {
-        'project_id': 'Projekt 1',
-        'session_data':
-                    {'Session Date': 'Datum und Uhrzeit', 'Session time': 10000, 'topic': 'Todo-List point'}
-        }
-}
+with a sql Database:
+Database: Projekte:_Tracker
+        Table: Projekt 1
+        Table: Projekt 2
+        ...
+            Daten: [Date, time, task]
+            ...
 
+projects:
+      project_id (PK)
+      project_name
+
+sessions:
+      session_id(PK)
+      project_id(FK)
+      time_spent
+      date
+      task
+
+      
++---------------------+           +-------------------+
+|      projects       |           |      sessions     |
++---------------------+           +-------------------+
+| project_id (PK)     |<----------| session_id (PK)   | 
+| project_name        |           | project_id (FK)   |
++---------------------+           | session_date      |
+                                  | time_spent        |
+                                  | task              |
+                                  +-------------------+
+
++---------------------+           +-------------------+            
+|      projects       |           |      sessions     |            
++---------------------+           +-------------------+            
+| project_id (PK)     |<----------| session_id (PK)   |            
+| project_name        |           | project_id (FK)   |            +-------------------+
++---------------------+           | session_date      |            |      task         |
+                                  | time_spent        |            +-------------------+
+                                  | task_id (FK)      |----------->|  task_id (PK)     |
+                                  +-------------------+            |  task_description |
+                                                                   +-------------------+
 
 '''
-session_data1: dict  = {'Session Date' : '1 Datum und Uhrzeit',
-                        'Session time': 10000,
-                        'topic' : '1 Todo-List point'}
-session_data2: dict  = {'Session Date' : '2 Datum und Uhrzeit',
-                        'Session time': 20000,
-                        'topic' : '2 Todo-List point'}
-project_dict1: dict  = {'project_name' : 'Projekt 1', 'session_data' : [session_data1, session_data2]}
-project_dict2: dict  = {'project_name' : 'Projekt 1', 'session_data' : [session_data1, session_data2]}
-projects_dict: dict  = {'projects' : [project_dict1, project_dict2]}
 
-print(projects_dict)
-
-dictionary:dict  = {
-    "projects": [
-      {
-        "project_name": "Projekt 1",
-        "session_data": [
-          {
-            "Session Date": "1 Datum und Uhrzeit",
-            "Session time": 10000,
-            "topic": "1 Todo-List point"
-          },
-          {
-            "Session Date": "2 Datum und Uhrzeit",
-            "Session time": 20000,
-            "topic": "2 Todo-List point"
-          }
-        ]
-      },
-      {
-        "project_name": "Projekt 1",
-        "session_data": [
-          {
-            "Session Date": "1 Datum und Uhrzeit",
-            "Session time": 10000,
-            "topic": "1 Todo-List point"
-          },
-          {
-            "Session Date": "2 Datum und Uhrzeit",
-            "Session time": 20000,
-            "topic": "2 Todo-List point"
-          }
-        ]
-      }
-    ]
-  }
-
-if dictionary == projects_dict:
-    print(True)
+import sqlite3
