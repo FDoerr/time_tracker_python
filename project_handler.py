2f18900e3c0af111cd0030074fbd6cb1c6aca8c7
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-10-22 14:31:33 +0200

    added functions for updating db entries

--- project_handler.py
+++ project_handler.py
@@ -28,9 +28,10 @@ Database:
                                                                    +------------------+
 
 '''
-
+#TODO: Refactor everything to use f-strings, this also means run_sql_command/query won't need the data tuples anymore
+#TODO: modularize this Script
 import sqlite3 as sql3
-from typing import Any, Optional
+from typing import Any, Never, Optional
 from datetime import datetime
 
 #region sql execution functions
@@ -52,8 +53,12 @@ def run_sql_command(db_path:str, cmd:str, data:Optional[tuple] = None) -> None:
         
         connection.commit()
 
+    except sql3.OperationalError as operational_error:
+        print(f'Operational Error occured: {operational_error}')
     except sql3.IntegrityError as integrity_error:
         print(f'Integrity Error occured: {integrity_error}')
+    except Exception as error:
+        print(f'Unexpected Error occured: {error}')
 
     finally:        
         if cursor:
@@ -91,28 +96,7 @@ def run_sql_query(db_path:str, cmd:str, data:Optional[tuple] = None)  -> list[di
             connection.close()
     
     return results
-
-
-#region create triggers
-def create_trigger_check_project_id_on_session_insert() -> None:
-
-    cmd_create_trigger_check_project_id_on_session_insert:str = '''
-                                                                CREATE TRIGGER IF NOT EXISTS
-                                                                id_trigger
-                                                                BEFORE INSERT ON sessions
-                                                                FOR EACH ROW
-                                                                WHEN NEW.task_id IS NOT NULL
-                                                                BEGIN
-                                                                    SELECT CASE
-                                                                        WHEN((SELECT project_id FROM tasks WHERE task_id = NEW.task_id) != NEW.project_id)
-                                                                        THEN RAISE (ABORT, 'task does not belong to the same project_id as the session.')
-                                                                    END;
-                                                                END;
-                                                                '''
-    
-    run_sql_command('time_tracker_data.db', cmd_create_trigger_check_project_id_on_session_insert)
 #endregion
-
 
 #region create tables
 def create_projects_table() -> None:
@@ -160,7 +144,30 @@ def create_tasks_table() -> None:
 
     run_sql_command('time_tracker_data.db', cmd_create_tasks_table)
     
-#endregion 
+#endregion
+
+ 
+#region create triggers
+#TODO: create triggers for UPDATE consistency
+
+def create_trigger_check_project_id_on_session_insert() -> None:
+
+    cmd_create_trigger_check_project_id_on_session_insert:str = '''
+                                                                CREATE TRIGGER IF NOT EXISTS
+                                                                id_trigger
+                                                                BEFORE INSERT ON sessions
+                                                                FOR EACH ROW
+                                                                WHEN NEW.task_id IS NOT NULL
+                                                                BEGIN
+                                                                    SELECT CASE
+                                                                        WHEN((SELECT project_id FROM tasks WHERE task_id = NEW.task_id) != NEW.project_id)
+                                                                        THEN RAISE (ABORT, 'task does not belong to the same project_id as the session.')
+                                                                    END;
+                                                                END;
+                                                                '''
+    
+    run_sql_command('time_tracker_data.db', cmd_create_trigger_check_project_id_on_session_insert)
+#endregion
 
 
 #region add to tables
@@ -245,30 +252,31 @@ def fetch_tasks(project_id:int) -> list[dict]:
 
 #region delete entries
 
-def del_project(project_id) -> None:
+def del_project(project_id:int) -> None:
     
     cmd_del_project:str = '''
                           DELETE FROM projects 
-                          WHERE project_id=?
+                          WHERE project_id = ?;
                           '''
     
     run_sql_command('time_tracker_data.db', cmd_del_project, (project_id,))
 
-def del_task(task_id) -> None:
+
+def del_task(task_id:int) -> None:
 
     cmd_del_task:str = '''
-                          DELETE FROM tasks 
-                          WHERE task_id=?
-                          '''
+                       DELETE FROM tasks 
+                       WHERE task_id = ?;
+                       '''
     
     run_sql_command('time_tracker_data.db', cmd_del_task, (task_id,))
 
 
-def del_session(session_id) -> None:
+def del_session(session_id:int) -> None:
 
     cmd_del_session:str = '''
                           DELETE FROM sessions 
-                          WHERE session_id=?
+                          WHERE session_id = ?;
                           '''
     
     run_sql_command('time_tracker_data.db', cmd_del_session, (session_id,))
@@ -277,14 +285,102 @@ def del_session(session_id) -> None:
 
 
 
-#region change entries
-#TODO
+#region update entries
+
+def update_project(new_project_name:str, project_id:int) -> None:
+
+    cmd_update_project:str = '''
+                             UPDATE projects
+                             SET project_name = ?
+                             WHERE project_id = ?;
+                             '''
+    
+    run_sql_command('time_tracker_data.db', cmd_update_project, (new_project_name, project_id))
+
+
+def update_session(session_id:      int,
+                   new_project_id:  Optional[int] = None,
+                   new_session_date:Optional[str] = None,
+                   new_time_spent:  Optional[int] = None,
+                   new_task_id:     Optional[int] = None) -> None:
+
+    building_blocks_cmd:list[str] = []
+    cmd_update_session = '''
+                      UPDATE sessions
+                      SET
+                      '''
+    
+    if new_project_id is not None:
+        building_blocks_cmd.append(f' project_id = {new_project_id}')        
+
+    if new_session_date is not None:
+        building_blocks_cmd.append(f' session_date = "{new_session_date}"')
+
+    if new_time_spent is not None:
+        building_blocks_cmd.append(f' time_spent = {new_time_spent}')
+
+    if new_task_id is not None:
+        building_blocks_cmd.append(f' task_id = {new_task_id}')
+    
+
+    if building_blocks_cmd == []:
+       raise ValueError('At least one Optional field must be provided to update the session.')        
+
+    cmd_update_session += ','.join(building_blocks_cmd)
+    cmd_update_session += f' WHERE session_id = {session_id};'    
+    
+    run_sql_command('time_tracker_data.db', cmd_update_session)
+
+
+def update_task(task_id:int, new_project_id:Optional[int] = None , new_task_description:Optional[str] = None) -> None:
+    
+    building_blocks_cmd:list[str] = []
+
+    cmd_update_task = '''
+                      UPDATE tasks
+                      SET
+                      '''
+    
+    if new_project_id is not None:
+        building_blocks_cmd.append(f' project_id = {new_project_id}')        
+
+    if new_task_description is not None:
+        building_blocks_cmd.append(f' task_description = "{new_task_description}"')
+    
+    if building_blocks_cmd == []:
+       raise ValueError('At least one Optional field must be provided to update the task.')
+        
+
+    cmd_update_task += ','.join(building_blocks_cmd)
+    cmd_update_task += f' WHERE task_id = {task_id};'    
+    
+    run_sql_command('time_tracker_data.db', cmd_update_task)
+
+    
+def update_task_description(task_id:int, new_task_description:str) -> None:
+    #simplified version of update tasks which might be more usefull
+    cmd_update_task_description = '''
+                                  UPDATE tasks
+                                  SET task_description = ?
+                                  WHERE task_id = ?;
+                                  '''
+    
+    run_sql_command('time_tracker_data.db', cmd_update_task_description, (new_task_description, task_id))
 
 #endregion
+
+#region if __name__=='__main__':
 if __name__=='__main__':
     print(fetch_projects())
     print(fetch_sessions(1))
     print(fetch_tasks(1))
+    update_project('Project_name changed', 1)
+    update_task(1, 1)
+    update_session(2, 2, 'new date', 666, 1)
+    #update_task(3, new_project_id = 2)
+    #update_task(1, 2, 'test')    
+    #update_task(2, new_task_description='test')   
+    #update_task(1)
     # #del_project(1)
     # #del_session(1)
     # create_projects_table()
