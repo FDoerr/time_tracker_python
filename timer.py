2ae88d70b1986c0a074930a97264fe59c6428bbf
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-10-23 11:14:23 +0200

    added return types to methods

--- timer.py
+++ timer.py
@@ -2,29 +2,29 @@ import time
 
 class Timer:   
         
-    def __init__(self):
-        self.running: bool = False
-        self.start_time: float   = 0.0
+    def __init__(self) -> None:
+        self.running:      bool  = False
+        self.start_time:   float = 0.0
         self.current_time: float = 0.0
         self.elapsed_time: float = 0.0
 
-    def start(self):
+    def start(self) -> None:
         self.start_time = time.time() - self.elapsed_time
-        self.running = True      
+        self.running    = True      
 
-    def stop(self):
+    def stop(self) -> None:
         self.running= False
 
-    def reset(self):
-        self.start_time = 0.0
+    def reset(self) -> None:
+        self.start_time   = 0.0
         self.current_time = 0.0
         self.elapsed_time = 0.0
 
-    def get_elapsed_time(self):
+    def get_elapsed_time(self) -> int:
         self.update_timer()
         self.elapsed_time = int(self.current_time - self.start_time)
         return self.elapsed_time
 
-    def update_timer(self):
+    def update_timer(self) -> None:
         if self.running == True:            
             self.current_time = time.time()   
