54e32ef1380b5d53972352a073bcd09ea9b0f28d
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-11-06 10:35:37 +0100

    added setup_database function

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from gui import root
+import project_handler as db
+
+
+def setup_database():
+    db.create_projects_table()
+    db.create_tasks_table()
+    db.create_sessions_table()
+    db.create_trigger_check_project_id_on_session_insert()
+    db.create_trigger_check_project_id_on_session_update()
 
 
 def main():
+    setup_database()
     root.mainloop()
 
 
