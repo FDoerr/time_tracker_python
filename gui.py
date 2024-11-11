905ea42ab614b21a28719d2989ca7faa7374c405
FDoerr <183077623+FDoerr@users.noreply.github.com>
2024-11-11 15:16:20 +0100

    refactored placement of GUI

--- gui.py
+++ gui.py
@@ -384,7 +384,6 @@ style.configure('.', font=('Helvetica', 12))
 #region project UI Elements
 #project display frame
 project_display_frame = ttk.Frame(root)
-project_display_frame.grid(row=1, column=1, padx=10, pady=10, sticky=tk.NW)
 # project_title_combobox
 project_name = tk.StringVar(value= 'Project name')
 project_title_combobox = ttk.Combobox(project_display_frame,
@@ -393,46 +392,40 @@ project_title_combobox = ttk.Combobox(project_display_frame,
                                       height       = 5,
                                       postcommand  = click_projects_combobox)
 project_title_combobox.bind('<<ComboboxSelected>>', click_update_task_and_session_display)
-project_title_combobox.pack(side=tk.LEFT, padx=10)
 project_title_combobox.project_dict = dict() #adds project_dict attribute for future reference
 # add project button
 add_project_button = ttk.Button(project_display_frame,
                                 text    = 'Add Project',
                                 command = add_project)
-add_project_button.pack(side=tk.LEFT, padx=10)
 # delete project button
 delete_project_button = ttk.Button(project_display_frame,
                                    text    = 'delete Project',
                                    command = del_project)
-delete_project_button.pack(side=tk.LEFT, padx=10)
+
 #endregion
 
 
 #region total time label UI Elements
 # subframe to group total time labels
 frame_total_time =ttk.Frame(root)
-frame_total_time.grid(row=2, column=1, padx = 50, pady = 10, sticky=tk.EW)
 # total_time_label_name
 total_time_label_name = ttk.Label(frame_total_time, text = 'Total: ')
-total_time_label_name.pack(padx = 10, pady = 10)
-
 # total_time_label
 total_time_style = Style()
 total_time_style.configure('total_time_style.TLabel',
                            font=('helvetica', 15, 'bold'))
 total_time = tk.StringVar(value= 'dd:hh:mm:ss')
-total_time_label = ttk.Label(frame_total_time, textvariable = total_time, style='total_time_style.TLabel')
-total_time_label.pack(padx = 10, pady = 10)
+total_time_label = ttk.Label(frame_total_time,
+                             textvariable = total_time,
+                             style        = 'total_time_style.TLabel')
 #endregion
 
 
 #region todo list UI Elements
 # frame for treeview & scrollbar
 task_frame = ttk.LabelFrame(root, text='Tasks')
-task_frame.grid(row=2, column=1, padx=10, pady=5, sticky=tk.W)
 # task tree frame
 task_tree_frame = ttk.Frame(task_frame)
-task_tree_frame.pack(side=tk.TOP, padx=10, pady=5)
 # treeview
 task_list_tree_columns: tuple = ('Done: ', 'ToDo: ', 'task_done', 'task_id') # !be carefull changing any of these, display relies  on the order of these
 task_list_tree = ttk.Treeview(task_tree_frame,
@@ -450,40 +443,32 @@ task_list_scrollbar = ttk.Scrollbar(task_tree_frame,
                                     orient  = tk.VERTICAL,
                                     command = task_list_tree.yview)
 task_list_tree.configure(yscrollcommand=task_list_scrollbar.set)
-# Place  treeview and scrollbar
-task_list_tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-task_list_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 # task button frame
 task_button_frame = ttk.Frame(task_frame)
-task_button_frame.pack(side=tk.BOTTOM, padx=0, pady=0)
 # add task button
 add_task_button = ttk.Button(task_button_frame,
                              width   = 10,
                              text    = 'Add Task',                              
                              command = add_task)
-add_task_button.grid(column=1, row = 1, sticky=tk.NW, padx=5, pady=5)
 # delete task button
 delete_task_button = ttk.Button(task_button_frame,
                                 width   = 10,
                                 text    = 'Delete Task',
                                 command = del_task)
-delete_task_button.grid(column=2, row = 1,sticky=tk.NE, padx=5, pady=5)
 # toggle task done button
 toggle_task_done_button = ttk.Button(task_button_frame,
                                      width   = 10,
                                      text    = 'Toggle Done',
                                      command = toggle_task_done)
-toggle_task_done_button.grid(column=1, row = 2, sticky=tk.S, padx=5, pady=5)
+
 #endregion
 
 
 #region session log UI Elements
 # frame for treeview & scrollbar
 log_frame = ttk.Labelframe(root, text='logs')
-log_frame.grid(row=6, column=1, padx=10, pady=10)
 # log tree & scrollbar frame
 log_tree_frame = ttk.Frame(log_frame)
-log_tree_frame.pack(side=tk.TOP, padx=5, pady=5)
 # treeview
 log_tree_column:tuple = ('Date: ',
                          'Duration: ',
@@ -508,21 +493,17 @@ log_scrollbar = ttk.Scrollbar(log_tree_frame,
                               orient=tk.VERTICAL,
                               command=log_tree.yview)
 log_tree.configure(yscrollcommand=log_scrollbar.set)
-# Place  treeview and scrollbar
-log_tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-log_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 #delete session button
 delete_session_button = ttk.Button(log_frame,
                                    text    = 'Delete Session',
                                    command = del_session)
-delete_session_button.pack(side=tk.BOTTOM, padx=5, pady=5)
+
 #endregion
 
 
 #region Timer Button UI Elements
 # reset & save frame
 frame_reset_save = ttk.Frame(root)
-frame_reset_save.grid(row=2, column=1, padx=10, pady=10, sticky=tk.E)
 # session_time_button
 session_time_button_style = Style()
 session_time_button_style.configure('session_time_button_style.TButton',
@@ -532,17 +513,52 @@ session_time_button = ttk.Button(frame_reset_save,
                                  width   = 12,                                 
                                  text    = timer_button_default_text,
                                  command = press_timer_button)
-session_time_button.pack(side=tk.LEFT, padx = 10, pady = 10, fill=tk.BOTH)
 # reset_button
 reset_button = ttk.Button(frame_reset_save,
                           text    = 'Reset',
                           command = reset)
-reset_button.pack(side=tk.BOTTOM, padx = 10, pady = 10)
 # save_button
 save_button = ttk.Button(frame_reset_save,
                          text    = 'Save',
                          command = add_session)
-save_button.pack(side=tk.TOP, padx = 10, pady = 10)
+
 #endregion
 
+#region placing GUI elements
+# project_display_frame
+project_display_frame.grid(row=1, column=1, padx=10, pady=10, sticky=tk.NW)
+project_title_combobox.pack(side=tk.LEFT, padx=10)
+add_project_button.pack(    side=tk.LEFT, padx=10)
+delete_project_button.pack( side=tk.LEFT, padx=10)
+
+# frame_total_time
+frame_total_time.grid(row=2, column=1, padx=50, pady=10, sticky=tk.EW)
+total_time_label_name.pack(padx=10, pady=10)
+total_time_label.pack(     padx=10, pady=10)
+
+# frame task tree
+task_frame.grid(row=2, column=1, padx=10, pady=5, sticky=tk.W)
+task_tree_frame.pack(side=tk.TOP, padx=10, pady=5) # parent: task_frame
+# Place  treeview and scrollbar
+task_list_tree.pack(     side=tk.LEFT, fill=tk.BOTH, expand=True)
+task_list_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+# task button frame
+task_button_frame.pack(side=tk.BOTTOM, padx=0, pady=0) 
+add_task_button.grid(        column=1, row=1, sticky=tk.NW, padx=5, pady=5)
+delete_task_button.grid(     column=2, row=1, sticky=tk.NE, padx=5, pady=5)
+toggle_task_done_button.grid(column=1, row=2, sticky=tk.S,  padx=5, pady=5)
+
+# frame log tree
+log_frame.grid(row=6, column=1, padx=10, pady=10)
+log_tree_frame.pack(side=tk.TOP, padx=5, pady=5) # parent: log_frame
+# Place  treeview and scrollbar
+log_tree.pack(             side=tk.LEFT,  fill=tk.BOTH, expand=True)
+log_scrollbar.pack(        side=tk.RIGHT, fill=tk.Y)
+delete_session_button.pack(side=tk.BOTTOM, padx=5, pady=5)
+
+# reset & save frame
+frame_reset_save.grid(row=2, column=1, padx=10, pady=10, sticky=tk.E)
+session_time_button.pack(side=tk.LEFT,   padx = 10, pady = 10, fill=tk.BOTH)
+reset_button.pack(       side=tk.BOTTOM, padx = 10, pady = 10)
+save_button.pack(        side=tk.TOP,    padx = 10, pady = 10)
 #endregion GUI setup
